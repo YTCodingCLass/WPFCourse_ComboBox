5c7fc4073f96f4c3cdbbfd04c188b8b0434dc277
YTCodingClass <familiekamhieh@gmail.com>
2021-09-29 18:05:10 +0200

    Add project files.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int i = 3;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Add_Click(object sender, RoutedEventArgs e)
+        {
+            i += 1;
+            cb_selection.Items.Add($"Test{i}");
+        }
+
+        private void btn_Show_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedItem = (string) cb_selection.SelectedItem;
+            MessageBox.Show($"Your Selection is: {selectedItem}");
+        }
+
+        private void cb_selection_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedItem = e.AddedItems[0];
+            if(selectedItem != null)
+            {
+                MessageBox.Show($"Your Selection is: {selectedItem}");
+            }
+        }
+    }
+}
